1d089282fcb76bc4c1ebb370723f6f375a1948d9
silence394 <silence394@126.com>
2018-06-03 18:57:04 +0800

    Add tank learn demo

---
+++ Unreal/Tank/tank/Source/tank/tankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "tankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_API AtankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
