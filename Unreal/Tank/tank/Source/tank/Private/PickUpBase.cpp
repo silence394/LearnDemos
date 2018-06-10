c76316f2fed2aa8fe7feb8d2db167fdb212ec514
silence394 <silence394@126.com>
2018-06-10 22:13:10 +0800

    Delete log output

--- Unreal/Tank/tank/Source/tank/Private/PickUpBase.cpp
+++ Unreal/Tank/tank/Source/tank/Private/PickUpBase.cpp
@@ -36,7 +36,6 @@ void APickUpBase::Tick(float DeltaTime)
 void APickUpBase::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	UseItem(OtherActor);
-	UE_LOG(LogTemp, Warning, TEXT("on OVERLAP!!"));
 }
 
 void APickUpBase::UseItem(AActor* actor)
